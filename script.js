d2ff52f9485cb8eb4c64cdebfc74f8913d0b910d
Jens Natzschka <jens.natzschka@posteo.de>
2024-10-23 14:23:01 +0200

    added gui

--- script.js
+++ script.js
@@ -6,15 +6,35 @@ window.addEventListener('load', init);
 
 
 function init() {
-    document.getElementById('button').addEventListener('click', startGame);
+    document.getElementById('btn_start').addEventListener('click', startGame);
+    document.getElementById('btn_instructions').addEventListener('click', function() {
+        toggle('instructions');
+      });
+      document.getElementById('btn_instructions_back').addEventListener('click', function() {
+        toggle('instructions');
+      });
+      document.getElementById('btn_imprint').addEventListener('click', function() {
+        toggle('imprint');
+      });
+      document.getElementById('btn_imprint_back').addEventListener('click', function() {
+        toggle('imprint');
+      });
     document.getElementById('mute').addEventListener('click', mutePage)
 }
 
 function startGame(){
     canvas = document.getElementById('canvas');
+    canvas.classList.remove('d-none');
+    document.getElementById('controls').classList.add('d-none');
     keyboard = new Keyboard();
     world = new World(canvas, keyboard, level1);
 };
+
+function toggle(section) {
+    document.getElementById(section).classList.toggle('d-none');
+    document.getElementById('controls').classList.toggle('d-none');
+    document.getElementById('wrapper').classList.toggle('bg-grey');
+}
 
 function mutePage() {
     world.toggleMuteAll();
